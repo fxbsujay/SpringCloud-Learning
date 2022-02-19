078c6f22c5f85d99d3a5d761b0782509b2d440bc
fxbsujay <fxbsujay@gmail.com>
2022-02-19 21:42:29 +0800

    初始化

---
+++ cloud-provider-payment-8001/src/main/java/com/susu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.susu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sujay
+ * @version   21:38 2022/2/19
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+
+}
