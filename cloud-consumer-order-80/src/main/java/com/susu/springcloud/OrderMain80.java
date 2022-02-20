0ed1bb7e719ddd521db67f506d211a01d7356c95
fxbsujay <fxbsujay@gmail.com>
2022-02-20 15:47:46 +0800

    Eureka集群

--- cloud-consumer-order-80/src/main/java/com/susu/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/susu/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.susu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author sujay
  * @version 23:53 2022/2/19
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- cloud-eureka-server-7002/src/main/java/com/susu/springcloud/EurekaMain7002.java
+++ cloud-eureka-server-7002/src/main/java/com/susu/springcloud/EurekaMain7002.java
@@ -2,18 +2,16 @@ package com.susu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author sujay
- * @version 21:38 2022/2/19
+ * @version 15:30 2022/2/20
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
-
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
-
 }
