8c61205e09385f3fb3e719a45b1f5b5985ba230b
fxbsujay <fxbsujay@gmail.com>
2022-02-22 00:17:38 +0800

    踩坑日记，新版Eureka-client把ribbon移除了，使用ReactorLoadBalancer作为负载均衡器

--- cloud-consumer-order-80/src/main/java/com/susu/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/susu/springcloud/OrderMain80.java
@@ -1,16 +1,19 @@
 package com.susu.springcloud;
 
+import com.susu.springcloud.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 
 /**
  * @author sujay
  * @version 23:53 2022/2/19
  */
 @SpringBootApplication
-@EnableEurekaClient
+@LoadBalancerClients(defaultConfiguration = {MyConfig.class})
 public class OrderMain80 {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-consumer-order-80/src/main/java/com/susu/springcloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/susu/springcloud/controller/OrderController.java
@@ -3,6 +3,12 @@ package com.susu.springcloud.controller;
 import com.susu.springcloud.entity.Payment;
 import com.susu.springcloud.entity.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.lang.reflect.ParameterizedType;
 
 /**
  * @author sujay
@@ -27,14 +34,23 @@ public class OrderController {
     @Resource
     public RestTemplate restTemplate;
 
+    @Autowired
+    public LoadBalancerClient loadBalancerClient;
+
     @GetMapping("payment/save")
     public Result<Payment> save(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL ,payment,Result.class);
     }
 
-    @GetMapping("payment/info/{id}")
-    public Result<Payment> info(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(PAYMENT_URL + "/payment/" + id,Result.class);
+    @GetMapping("payment/{id}")
+    public Result<Payment> get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/" + id, Result.class);
     }
+
+   /* @GetMapping("payment/info/{id}")
+    public Result<Payment> info(@PathVariable("id") Long id) {
+        loadBalancerClient.execute(PAYMENT_URL + "/payment/" + id, HttpMethod.GET,null,new ParameterizedTypeReference<Result<Payment>>(){})
+
+    }*/
 
 }
