7802a8aefbf558a09e10f1e4726e55610c2c50bc
fxbsujay <fxbsujay@gmail.com>
2022-04-19 23:18:28 +0800

    nacos

---
+++ cloud-alibaba-provider-payment-9001/src/main/java/com/susu/springcloud/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.susu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author sujay
+ * @version 21:38 2022/2/19
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+
+}
