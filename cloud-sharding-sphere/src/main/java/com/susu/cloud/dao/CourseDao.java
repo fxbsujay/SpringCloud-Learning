2a69d03b9e3141b2a008bd197d0331ccc0cd0fdf
fxbsujay <fxbsujay@gmail.com>
2022-05-06 23:47:18 +0800

    Sharding-jdbc

--- cloud-sharding-sphere/src/main/java/com/susu/cloud/dao/CourseDao.java
+++ cloud-sharding-sphere/src/main/java/com/susu/cloud/dao/CourseDao.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.susu.cloud.entity.Course;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author fxbsujay@gmail.com
+ */
 @Repository
 public interface CourseDao extends BaseMapper<Course> {
 
