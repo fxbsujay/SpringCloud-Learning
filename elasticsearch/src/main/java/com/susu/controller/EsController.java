da6fcab8ed9293ae8ed0e5889bff3a02ab79773b
fxbsujay <fxbsujay@gmail.com>
2024-02-21 18:10:51 +0800

    Es

--- elasticsearch/src/main/java/com/susu/controller/EsController.java
+++ elasticsearch/src/main/java/com/susu/controller/EsController.java
@@ -1,7 +1,13 @@
 package com.susu.controller;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexResponse;
+import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
 import co.elastic.clients.json.JsonpMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -9,11 +15,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("es")
+@Slf4j
 public class EsController {
-
 
     @Autowired
     private ElasticsearchOperations operations;
@@ -27,10 +34,25 @@ public class EsController {
     @Autowired
     private JsonpMapper mapper;
 
-    @GetMapping("query")
-    public ResponseEntity<String> query() {
+    /**
+     * 索引
+     */
+    @GetMapping("createIndex")
+    public ResponseEntity<String> createIndex() throws IOException {
+        String index = "shopping";
 
-        return ResponseEntity.ok("");
+        CreateIndexResponse createResponse = elasticsearchClient.indices().create(c -> c.index(index));
+        log.info("======createIndex====== index: {}, {}", createResponse.index(), createResponse.acknowledged());
+
+        GetIndexResponse geResponse = elasticsearchClient.indices().get(c -> c.index(index));
+        log.info("======queryIndex====== result: {}", geResponse.result());
+
+        DeleteIndexResponse deleteResponse = elasticsearchClient.indices().delete(c -> c.index(index));
+        log.info("======deleteIndex====== result: {}", deleteResponse.acknowledged());
+
+        return ResponseEntity.ok( index);
     }
+
+
 
 }
